9230333d2d3fb931811ea418f077069b415defda
Edelmeyder <edelmiro.7.8.9.5@gmail.com>
2022-08-02 17:11:39 -0300

    RTC library started

--- rtc.c
+++ rtc.c
@@ -2,6 +2,37 @@
 #include "stm32f10x.h"
 
 
+void _sendData(uint8_t);
+uint8_t _receiveData(void); 
+
+
 void RTC_init() {
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN; // Enables port B clock
+	RCC->APB1ENR |= RCC_APB1ENR_SPI2EN; // Enables SPI2 clock
 	
+	// Pin B15:MOSI/MISO bidirectional as alternate function - Pin B13:SCL as alternate function - Pin B12:SS output software controlled
+	GPIOB->CRH = (GPIOB->CRH & 0xBFB3FFFF) | 0xB0B30000;
+	GPIOB->ODR &= (uint16_t)~(1<<12); // Sets SS to low
+	
+	SPI2->CR1 = 0xC3EC; /*
+	Bidirectional 1-line
+	No CRC
+	8-bit dataframe LSF
+	NSS pin free
+	Master mode
+	Clock low on idle
+	BR = 5
+	SPI enabled
+	*/
+}
+
+void _sendData(uint8_t data){
+	SPI2->DR = data; // Loads data to Tx
+	while((SPI2->SR & (1<<1)) == 0); // Waits for transmission to end (TXE=1)
+}
+
+uint8_t _receiveData(){
+	SPI2->CR1 &= (uint16_t)~(1<<14); // Starts receiving data
+	while((SPI2->SR & (1<<0)) == 0); // Waits for reception to end (RXNE=1)
+	return (uint8_t)SPI2->DR;
 }
--- rtc.h
+++ rtc.h
@@ -1,6 +1,8 @@
 #ifndef _RTC_H
 #define _RTC_H
 
+// Library for the DS1302 RTC module with 3wire SPI communication
+
 void RTC_init(void);
 
 #endif
